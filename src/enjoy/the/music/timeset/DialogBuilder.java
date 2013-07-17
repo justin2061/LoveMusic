2839b54b5badd889a504dbe66849c2aa0587071c
beginnerjyh <nocturner@sina.com>
2013-07-18 01:47:06 +0800

    first commit

---
+++ src/enjoy/the/music/timeset/DialogBuilder.java
@@ -0,0 +1,26 @@
+package enjoy.the.music.timeset;
+
+
+
+
+
+import enjoy.the.music.R;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.view.ContextThemeWrapper;
+
+/**设置弹出式对话框的样式，即可继承的对话框样本**/
+
+
+public class DialogBuilder {
+	private static Builder instance;
+	private static ContextThemeWrapper localContextThemeWrapper;
+
+	public static Builder getInstance(Context context) {
+		localContextThemeWrapper = new ContextThemeWrapper(context,
+				R.style.Theme_Dialog);
+		instance = new AlertDialog.Builder(localContextThemeWrapper);
+		return instance;
+	}
+}
